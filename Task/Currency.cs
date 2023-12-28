204deba7f110cd4d0c24caa62693cfcaefaac39f
Taras Nevmerzhytskyi <kn222_ntv@student.ztu.edu.ua>
2023-12-28 16:22:55 +0200

    Створено клас Currency

---
+++ Task/Currency.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    public class Currency
+    {
+        protected string Name { get; set; }
+        protected double ExRate { get; set; }
+
+        public Currency()
+        {
+            
+        }
+
+        public Currency(string name, double exRate)
+        {
+            Name = name;
+            ExRate = exRate;
+        }
+
+        public Currency(Currency otherCurrency)
+        {
+            Name = otherCurrency.Name;
+            ExRate = otherCurrency.ExRate;
+        }
+    }
+}
